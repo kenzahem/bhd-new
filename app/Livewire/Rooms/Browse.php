4fcf4585a79f817ccbee17b682739637303be91d
Khen <kenzahem@gmail.com>
2024-09-11 16:47:03 +0800

    temporary remove loading test

--- app/Livewire/Rooms/Browse.php
+++ app/Livewire/Rooms/Browse.php
@@ -19,7 +19,7 @@ class Browse extends Component
 
     public function render()
     {
-        sleep(3);
+        // sleep(3);
         // $rooms = Room::paginate(8);
         return view('livewire.rooms.browse', [
             'rooms' => Room::where('title', 'like', '%'.$this->filter_search.'%')->paginate(6),
